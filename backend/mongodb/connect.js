d585fc9acad818f03a0c0d3b9b60e01aa1e7e3ab
Péter Révész <reveszpeter21@gmail.com>
2023-03-16 11:25:02 +0100

    add basic routing

--- backend/mongodb/connect.js
+++ backend/mongodb/connect.js
@@ -1,13 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = (url) => {
-  mongoose.set('strictQuery', true);
-  mongoose.connect(url)
-    .then(() => console.log('connected to mongo'))
-    .catch((err) => {
-      console.error('failed to connect with mongo');
-      console.error(err);
-    });
+    mongoose.set("strictQuery", true);
+    mongoose
+        .connect(url)
+        .then(() => console.log("MongoDB connected"))
+        .catch((err) => {
+            console.error(err);
+        });
 };
 
 export default connectDB;
